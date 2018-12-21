8cc65a426bc6729580674967faef5654c128593d
Rain Saarmäe <rain.saarmae@pipedrive.com>
2018-12-21 15:45:52 +0200

    1-2

--- 1/index.js
+++ 1/index.js
@@ -1,16 +1,36 @@
 const fs = require('fs');
 const readline = require('readline');
+const Set = require('collections/set');
 
 const lineReader = readline.createInterface({
 	input: fs.createReadStream('data.txt'),
 });
 
 let number = 0;
+const changes = [];
 
 lineReader
 	.on('line', function(line) {
-		number += parseInt(line, 10);
+		changes.push(parseInt(line, 10));
 	})
 	.on('close', () => {
-		console.log(number);
+		console.log(findDuplicate());
 	});
+
+function findDuplicate() {
+	const states = new Set([0]);
+	let previousStatesSize = -1;
+	let duplicateFound = false;
+	while (!duplicateFound) {
+		for (var change of changes) {
+			number += change;
+			states.add(number);
+			if (states.size === previousStatesSize) {
+				duplicateFound = true;
+				break;
+			}
+			previousStatesSize = states.size;
+		}
+	}
+	return number;
+}
